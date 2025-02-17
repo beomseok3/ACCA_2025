731e9c9e18d6db35220a025d3d634a63b9e0329a
bumwhale333c <bumwhale333c@gmail.com>
2025-02-17 13:14:09 +0900

    imu_encoder_tf

--- src/localization/localization/imu_encoder_odometry.py
+++ src/localization/localization/imu_encoder_odometry.py
@@ -7,6 +7,7 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TwistWithCovarianceStamped
 from tf_transformations import euler_from_quaternion, quaternion_from_euler
 import numpy as np
+import tf2_ros
 
 
 class Imu_Encoder(Node):
@@ -17,11 +18,13 @@ class Imu_Encoder(Node):
         self.declare_parameter("odom_topic", "/odom_imu_encoder")
         self.declare_parameter("frame_id", "odom")
         self.declare_parameter("child_frame_id", "base_link")
+        self.declare_parameter("logging", True)
 
         imu_topic = self.get_parameter("imu_topic").value
         odom_topic = self.get_parameter("odom_topic").value
         self.frame_id = self.get_parameter("frame_id").value
         self.child_frame_id = self.get_parameter("child_frame_id").value
+        self.logging = self.get_parameter("logging").value
 
         # subscriber
         self.imu_sub = self.create_subscription(
@@ -37,6 +40,11 @@ class Imu_Encoder(Node):
         # publisher(odometry)
         self.odom_pub = self.create_publisher(
             Odometry, odom_topic, qos_profile=qos_profile_system_default
+        )
+
+        # tf
+        self.tf_broadcaster = tf2_ros.TransformBroadcaster(
+            self, qos_profile=qos_profile_system_default
         )
 
         self.x = 0.0
@@ -69,29 +77,56 @@ class Imu_Encoder(Node):
 
         self.v_x = msg.twist.twist.linear.x
         self.v_y = msg.twist.twist.linear.y
-        print(f"{self.v_x}  {self.v_y}")
 
         self.x = self.x + float(self.v_x) * dt
         self.y = self.y + float(self.v_y) * dt
-        print(f"{self.x}    {self.y}    {dt}")
+
+        if self.logging is True:
+            print(f"position & time: {self.x}    {self.y}    {dt}")
+            print(f"velocity & orientation: {self.v_x}  {self.v_y}  {self.yaw}")
 
         msg = Odometry()
         msg.header.stamp = self.get_clock().now().to_msg()
+
         msg.header.frame_id = self.frame_id
         msg.child_frame_id = self.child_frame_id
+
         msg.pose.pose.position.x = self.x
         msg.pose.pose.position.y = self.y
+
         q = quaternion_from_euler(0, 0, self.yaw)
         msg.pose.pose.orientation.x = q[0]
         msg.pose.pose.orientation.y = q[1]
         msg.pose.pose.orientation.z = q[2]
         msg.pose.pose.orientation.w = q[3]
+
         self.odom_pub.publish(msg)
+
+        self.publish_tf(self.x, self.y, q)
+
+    def publish_tf(self, x, y, q):
+        tf_msg = tf2_ros.TransformStamped()
+        tf_msg.header.stamp = self.get_clock().now().to_msg()
+
+        tf_msg.header.frame_id = "odom"
+        tf_msg.child_frame_id = "base_link"
+
+        tf_msg.transform.translation.x = x
+        tf_msg.transform.translation.y = y
+        tf_msg.transform.translation.z = 0.0
+
+        tf_msg.transform.rotation.x = q[0]
+        tf_msg.transform.rotation.y = q[1]
+        tf_msg.transform.rotation.z = q[2]
+        tf_msg.transform.rotation.w = q[3]
+
+        self.tf_broadcaster.sendTransform(tf_msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
     imu_encoder_node = Imu_Encoder()
+
     rclpy.spin(imu_encoder_node)
     rclpy.shutdown()
 
